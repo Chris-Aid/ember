e547133c54464b688ded026299af4d7f074b9100
samuelbergen <kontakt@samuel-bergen.de>
2022-09-11 16:58:54 +0200

    create basic design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { EditorModule } from '@tinymce/tinymce-angular';
+
+import { AppComponent } from './app.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PrimaryViewContentComponent } from './primary-view-content/primary-view-content.component';
 import { SecondaryViewContentComponent } from './secondary-view-content/secondary-view-content.component';
 import { SearchComponent } from './search/search.component';
+import { MessageInputComponent } from './message-input/message-input.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,6 +23,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { MatTreeModule } from '@angular/material/tree';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +32,14 @@ import { MatTreeModule } from '@angular/material/tree';
     SidebarComponent,
     PrimaryViewContentComponent,
     SecondaryViewContentComponent,
-    SearchComponent
+    SearchComponent,
+    MessageInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    EditorModule,
 
     MatToolbarModule,
     MatIconModule,
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchComponent implements OnInit {
 
